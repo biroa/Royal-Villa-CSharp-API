d3aba4e845a9da0a3dcd791ef847c1a4746ed778
Adam Biro <adam.biro@gmail.com>
2026-05-14 19:14:02 +0200

    basic request header setings

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -18,18 +18,4 @@ public class VillaController : ControllerBase
     {
         return "Get Villas: " + id;
     }
-
-    [HttpGet("{id:int}/{name}")]
-    public string GetVillaById([FromRoute] int id,[FromRoute] string name)
-    {
-        return "Get Villas: " + id + " " + name;
-    }
-
-    [HttpGet("queryParams")]
-    public string GetVillaDataByQueryParams([FromQuery] int id,[FromHeader] string name)
-    {
-        return "Get Villas: " + id + " " + name;
-    }
-
-
 }
