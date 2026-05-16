232f82880be3f954325e58efc3204357dd4c693e
Adam Biro <adam.biro@gmail.com>
2026-05-16 13:38:41 +0200

    feat(database): add Villa entity and update AppDbContext
    Introduced the Villa entity with properties such as Id, CreatedDate, Details, ImageUrl, Name, Occupancy, Rate, Sqft, and UpdatedDate. Updated AppDbContext to include a DbSet for Villas, enhancing the database schema for villa management.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using RoyalVillaApi.Models;
 
 namespace RoyalVillaApi.Data;
 
@@ -6,5 +7,12 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    // public DbSet<YourEntity> YourEntities => Set<YourEntity>();
+    /// <summary>
+    /// DbSet<T> is an Entity Framework Core type that represents the Villas table
+    /// We say:
+    /// “There is a collection of Villa rows in the database.”
+    /// “Use this property to query, add, update, and remove them.”
+    /// “Conceptually it’s a set of many villas“
+    /// </summary>
+    public DbSet<Villa> Villas => Set<Villa>();
 }
